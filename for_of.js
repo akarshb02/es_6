05970cce7e4cda92a8759c6fd124eee2ae251690
Akarsh <akarshb02@gmail.com>
2021-03-16 19:32:33 +0530

    for of  loop

---
+++ for_of.js
@@ -0,0 +1,14 @@
+const colors = ['red', 'blue', 'yellow']
+
+for (let color of colors) {
+    console.log(color);
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+for (let number of numbers) {
+    console.log(number);
+}
+for(let items of numbers){
+    console.log(items);
+})
